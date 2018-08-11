c0e1b66e8b0e127d6d60c155eee9d24c69b42633
Peter Paluska <paluskapter@gmail.com>
2018-08-12 00:53:51 +0200

    Added scripts

---
+++ python/custom/sync.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+scp * pi:rpi_ws281x/python/custom
---
+++ python/custom/webserver.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo PYTHONPATH=".:/home/pi/rpi_ws281x/python/build/lib.linux-armv7l-2.7:${PYTHONPATH}" python /home/pi/rpi_ws281x/python/custom/app.py
